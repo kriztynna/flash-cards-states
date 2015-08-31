181efab9103d13d75297de3a49dd3b66a7a51a7a
Cristina <cristina.colon+git@gmail.com>
2015-08-31 13:34:00 -0400

    added menu

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,7 @@ var app = angular.module('flashCards', ['ui.router']);
 app.config(function($stateProvider,$urlRouterProvider){
 	$stateProvider
 		.state('statistics',{url:'/statistics',templateUrl:'../templates/statistics.html'})
-		.state('newcardform',{url:'/newcard',templateUrl:'../js/directives/newCard/NewCardForm.html'})
+		.state('newcard',{url:'/newcard',templateUrl:'../js/directives/newCard/NewCardForm.html'})
 		.state('viewcards',{url:'/viewcards',templateUrl:'../js/directives/flashCard/flashCardDirective.html'});
 });
 
